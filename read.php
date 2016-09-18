7755313105f3a05c416c217a2a92ab6892e01b77
stealstick <stillstick40@gmail.com>
2016-09-18 11:41:01 +0900

    read review

---
+++ read.php
@@ -0,0 +1,23 @@
+<?php
+include 'connect.php';
+
+$id = $_GET['id'];
+$array= array();
+$query=mysql_query("select name, title, text, time from review where id = '$id'");
+
+if(isset($id))
+{
+	while ($data = mysql_fetch_assoc($query)) {
+		array_push($array, array(
+		'name'=>$data[name],
+		'title'=>$data[title],
+		'time'=>$data[time],
+		));
+	}
+	echo json_encode($array);
+}
+else
+{
+	echo json_encode(array('read' => 'error'));
+}
+?>
